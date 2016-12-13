771a75eff328ee26f49bcfd6a03e4106b03450d6
Jahstus Travis Maat-hotep <skylinejm909979@gmail.com>
2016-12-13 13:42:49 -0800

    progress

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,19 @@
-// Write all your JavaScript code in this file!
-// You can use the namespace variable that's defined here.
+
 var namespace = "http://www.w3.org/2000/svg"
-var canvas = document.getElementById("game-board")
+var circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cx", 50)
+circle.setAttribute("cy", 50)
+circle.setAttribute("r", 10)
+canvas.appendChild(circle)
+var turn="player1"
+
+  function topLeft() }
+if(turn=="player1") }
+//Create circle in top left
+
+turn="player2"
+{else}
+//Creta rect in top left
+turn="player1"
+}
+}
